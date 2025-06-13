ed5cc71c2ab0a1f80421e134708e55c36da6e730
s1cht <sachnikitus@gmail.com>
2025-06-13 14:00:31 +0300

    Added premake file

---
+++ premake5.lua
@@ -0,0 +1,48 @@
+project "xxHash"
+	kind "StaticLib"
+	language "C"
+    staticruntime "off"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+        "xxhash.h",
+        "xxhash.c",
+        "xxh3.h",
+        "xxh_x86dispatch.h",
+        "xxh_x86dispatch.c",
+        "xxh_x86dispatch.c"
+    }
+
+    defines
+    {
+        "XXH_EXPORT",
+    }
+
+    includedirs
+    {
+        "./",
+    }
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C17"
+
+	filter "system:linux"
+		systemversion "latest"
+		cppdialect "gnu17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
